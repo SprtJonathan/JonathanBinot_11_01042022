5766c8646a60ed82096633e6c7dace625b6b9b42
Jonathan Binot <73881990+SprtJonathan@users.noreply.github.com>
2022-04-11 17:32:12 +0200

    Création de la page d'accueil en React

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,5 +1,27 @@
+import logo from "../assets/img/logo.jpg";
+import "../styles/Banner.css";
+
 function Banner() {
-    return <h1>KASA</h1>
+  return (
+    <div className="header">
+      <div className="banner">
+        <a className="website-logo-link">
+          <img className="website-logo" src={logo} alt="test"></img>
+        </a>
+        <nav className="menu">
+          <a className="menu--button" id="home">
+            Accueil
+          </a>
+          <a className="menu--button" id="about">
+            A Propos
+          </a>
+        </nav>
+      </div>
+      <div className="website-banner">
+        <h2 className="website-banner-text">Chez vous, partout et ailleurs</h2>
+      </div>
+    </div>
+  );
 }
 
-export default Banner
+export default Banner;
---
+++ src/components/Logements.js
@@ -0,0 +1,23 @@
+import logements from "../assets/data/logements.json";
+import "../styles/Logements.css";
+
+function LogementsList() {
+  return (
+    <div className="list">
+      {logements.map((mappedObject) => (
+        <div
+          id={`${mappedObject.id}`}
+          key={`${mappedObject.title}`}
+          className="thumb"
+          style={{
+            backgroundImage: `url(${mappedObject.cover})`,
+          }}
+        >
+          <h3 className="thumb--title">{mappedObject.title}</h3>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default LogementsList;
