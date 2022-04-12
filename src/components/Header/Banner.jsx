d1727c5ca5533ca2f99585980d664b0c2b0f8948
Jonathan Binot <73881990+SprtJonathan@users.noreply.github.com>
2022-04-12 18:44:00 +0200

    Fix de l'erreur empêchant l'affichage de l'image de fond du composant banner

--- src/components/Header/Banner.jsx
+++ src/components/Header/Banner.jsx
@@ -2,7 +2,12 @@ function Banner({ text, backgroundValue }) {
   return (
     <div
       className="website-banner"
-      style={{ background: `${backgroundValue}` }}
+      style={{
+        background: `${backgroundValue}`,
+        backgroundPosition: `center`,
+        backgroundRepeat: `no-repeat`,
+        backgroundSize: `cover`,
+      }}
     >
       <h2 className="website-banner-text">{text}</h2>
     </div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,13 +2,15 @@ import "../styles/Home.css";
 import Banner from "../components/Header/Banner";
 import LogementsList from "../components/Offers";
 
+import banniere from "../assets/img/Banner.png";
+
 function Home() {
   return (
     <div className="App">
       <Banner
         text="Chez vous, partout et ailleurs"
-        backgroundValue="linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.3)),
-    url(/../assets/img/Banner.png)"
+        backgroundValue={`linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.3)),
+    url(${banniere})`}
       />
       <LogementsList />
     </div>
